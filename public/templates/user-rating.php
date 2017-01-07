c63fc4686791d3193b5f3066958fa853047f205a
mukto90 <n.mukto@gmail.com>
2017-01-07 13:22:17 +0600

    license added

--- public/templates/user-rating.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * If you want to modify this template, please create a copy of this
- * file in "cbpr-templates" directory of you active theme
- */
-
-// $args
-
-echo apply_filters( 'cbpr_user_rating_html', $html, $post_id, $args );
